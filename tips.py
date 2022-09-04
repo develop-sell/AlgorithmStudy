d26155ab56b46a4cb45e3ce8a222eb8d52b0acfb
develop-sell <brianpark0500@gmail.com>
2022-09-04 18:04:51 +0900

    add: tips, tips_basic 추가

--- tips.py
+++ tips.py
@@ -15,7 +15,7 @@ print(map(lambda x: x > 0, numbers)) # --> [False, True, True, True]
 
 #-----------------------------------------------------------------------------------------#
 ### reduce: 누적적으로 함수에 적용시키는.
-from functools import reduce 
+from functools import reduce
 print(reduce(lambda x, y: x + y, [1, 3, 5, 7]))
 print(reduce(lambda x, y: y + x, 'abcde')) # 반대. #'edcba'
 # 1회차: x=1, y=3 -> 1+3 = 4
@@ -88,3 +88,56 @@ print(sum([int(i) for i in num])) # 6 출력
 num = 5
 print(bin(num)) # 출력: 0b101 (0b는 binary를 표시하는 방식.)
 print(bin(num).count('1')) # 2 
+
+
+#-----------------------------------------------------------------------------------------#
+### 전역변수 함수 내에서 사용법 global
+x = "awesome"
+def myfunc():
+    global x
+    x = "fantastic"
+myfunc()
+print("Python is " + x) # fantastic으로 변경되어 출력
+
+## => python은 함수 내에 있는 변수만 지역변수로 취급하고, 그 외 모두 전역변수 취급
+## 예제에서 c 변수가 for문 안에 선언되어있지만, 전역변수로 취급
+for i in range(3):
+    global_num = i
+print(global_num)
+
+#-----------------------------------------------------------------------------------------#
+### 큐, 스택
+from collections import deque
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.popleft()
+queue.append(7)
+
+queue.reverse() # 역순으로 바꾸기 
+print(queue) # 나중에 들어온 원소부터 출력 [7,2] (오른쪽부터 나가는 queue)
+
+### 스택 
+stack = []
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.pop()
+
+print(stack[::-1]) # 거꾸로 출력 [2,1] (왼쪽부터 나가는 stack)
+
+#-----------------------------------------------------------------------------------------#
+### 두번째로 큰 수 찾기
+# https://shoark7.github.io/programming/algorithm/second-largest-number-in-array
+def second_largest_number(arr):
+    second = largest = -float('inf') 
+    
+    for n in arr:
+        if n > largest:
+            second = largest
+            largest = n
+        elif second < n < largest:
+            second = n
+
+    return second
