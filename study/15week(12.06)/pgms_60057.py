4fa407a5d6fb9aa2b34dff64d0f900ff1ff50a9b
develop-sell <brianpark0500@gmail.com>
2022-12-05 01:18:34 +0900

    PGMS: 60057(문자열 압축) 2020 카카오 문제

---
+++ study/15week(12.06)/pgms_60057.py
@@ -0,0 +1,119 @@
+# 프로그래머스: 문자열 압축 
+# 4:49- 시간 오래 걸림.
+
+
+'''
+1,2,3, ... 이 순으로? 
+
+2개로 짜르면 무조건 2개만 가능.
+3개 단위로  짜르면 3개로만 가능한 것. 
+
+s/2 까지 진행. 
+'''
+
+# 해결 과정
+'''
+생각보다 어려웠다. 특히 케이스 5번처럼.
+맨 처음부터 나눠져야한다는 점이 이해하기 어려웠다.
+
+token화하는 것을 처음 배웠다. 
+wrap 사용할 것, 
+'''
+
+s = 'aabbaccc'
+# s = "ababcdcdababcdcd"
+# s = "abcabcdede"
+# s = "abcabcabcabcdededededede"
+s = "xababcdcdababcdcd"
+s = "aaaaaaaaaabbbbbbbbbb"
+s = '1'
+
+
+from textwrap import wrap
+import re
+
+ans = 1001
+for i in range(1,len(s)//2+1):
+    tokens = wrap(s, i)
+    cur = tokens[0]
+    restr = ""
+    recnt = 1
+    j = 0
+    while j < len(tokens)-1:
+        j += 1
+        if cur == tokens[j]:
+            recnt += 1
+        else:
+            if recnt > 1:
+                restr += str(recnt) + cur
+            else:
+                restr += cur
+            cur = tokens[j]
+            recnt = 1
+    
+    # 마지막 남은 것 넣어줄 것.
+    if recnt > 1:
+        restr += str(recnt) + cur
+    else:
+        restr += cur
+    ans = min(ans, len(restr))
+
+if ans == 1001:
+    # return len(s)
+    print(len(s))   
+else:
+    print(ans)   
+        
+
+
+# 첫번째 풀이
+'''
+s = 'aabbaccc'
+s = "ababcdcdababcdcd"
+s = "abcabcdede"
+s = "abcabcabcabcdededededede"
+s = "xababcdcdababcdcd" # 이게 안 풀리는데? 
+
+from collections import deque
+
+ans = 1001
+for i in range(1,len(s)//2):
+    # i가 길이 기준.
+    result = len(s)
+    qu = deque() # 기준 qu.
+    j = -1
+    while j < len(s)-1:
+        j += 1
+        # 원소 s[j]
+        if len(qu) < i:
+            qu.append(s[j])
+        else:
+            # 맨 앞 빼고. 뒤에 넣고.
+            # i+1, i+2 ... 등이랑 비교해서 같은지 확인? 
+            repeat = True
+            for k in range(1,i+1):
+                # indexoutofbound
+                if j+k >= len(s):
+                    repeat = False
+                    break
+                if qu[k-1] == s[j+k]:
+                    continue
+                else:
+                    repeat = False
+                    break
+                    
+            
+            if repeat:
+                j += i # 다음 j는 넘기기.
+                result -= i
+            else:
+                qu.popleft()
+                qu.append(s[j])
+                # 맨 앞 pop, 맨 뒤에 add? 
+                    
+    ans = min(ans, result)
+
+
+print(ans)
+
+'''
